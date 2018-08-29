9b6b45ebc4f43d1dad2ff8c04bb976fbc69d2cc5
Shao Heng <shtan@redharegames.com>
2018-08-29 16:38:11 +0800

    Packages installed

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+
+console.log(`Starting server.js`);
+
+
 const http = require('http');
 
 const hostname = '127.0.0.1';
@@ -6,8 +10,11 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Worlddskfbdsihbdividjfv \n');
+  res.end('Hello Worlddskfbdsihbdividjfv \n'); 
+
+
 });
+console.log(`Server created`);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
