6ee61389e371ce7b0e3b5465907d5f164e67af6e
Shao Heng <shtan@redharegames.com>
2018-08-30 18:42:13 +0800

    added modules

--- server.js
+++ server.js
@@ -9,12 +9,35 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 var app = express();
-app.get('/',function(req,res){
-  res.send('Hello World.  (express)');
-});
 
+var logger =function(req,res,next){
+  console.log('Logging');
+  next();
+}
+app.use(logger);
+
+//writing docu for body parser
+app.use(bodyParser.json);
+
+//place to put static resource like css files, jquery
+//set static path , 'public' is name of the folder
+//if an index.html is present in /public folder, it will always overwrite the home page of this server.js
+app.use(express.static(path.join(__dirname,'public')) ;
+
+//inorder not to rs the server everytime we make a change
+
+app.use(logger);
+
+
+
+//homepage
+app.get('/',function(req,res){
+  res.send('Home page. (Express server)');
+
+});
+//server listen fn
 app.listen(port,function(){
-  console.log('Server Started on Port:'+port+'...');
+  console.log('Server Started on Port:'+ port +',IP:'+ hostname +'  ...');
 });
 console.log(`Server created`);
 
